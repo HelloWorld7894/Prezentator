921dd73d2808dfaf3ae4462ade6ed127a54b0831
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-13 18:59:31 +0200

    app is done

--- src/gui/index.js
+++ src/gui/index.js
@@ -1,12 +1,9 @@
 const ipcRender = require("electron").ipcRenderer
 
 var presentation_data
-var n_presentations
+var n_presentation_rooms = 4 //TODO: make this configurable
 
-var pres_room_A = []
-var pres_room_B = []
-var pres_room_C = []
-var pres_room_D = []
+var room_iterators = [0, 0, 0, 0]
 
 //functions
 function handleFile(param){
@@ -44,9 +41,36 @@ function loadData(){
     ipcRender.send("data-request", "")
 
     ipcRender.on("data-send", (event, data) => {
-        data_arr = data.split(",")
-        presentation_data = data_arr
+        const arrays = data.split('\n').map(line => {
+            const matches = line.match(/\[(.*?)\]/g);
+            if (matches) {
+              return matches.map(match => {
+                const items = match.slice(1, -1).split(', ').map(item => item.trim());
+                const result = [];
+                for (let i = 0; i < items.length; i += 3) {
+                  result.push({
+                    title: items[i],
+                    name: items[i + 1],
+                    time: items[i + 2],
+                  });
+                }
+                return result;
+              });
+            }
+            return [];
+          });          
+        presentation_data = arrays
     })
+}
+
+function ConvertStringIntoTime(TimeString){
+    const TimeParts = TimeString.split(':');
+    const Time = new Date();
+    Time.setHours(parseInt(TimeParts[0], 10));
+    Time.setMinutes(parseInt(TimeParts[1], 10));
+    Time.setSeconds(0); //TODO: check
+
+    return Time
 }
 
 function refresh_presentations(){
@@ -54,18 +78,21 @@ function refresh_presentations(){
         return
     }
 
-    n_presentations = Math.floor(presentation_data.length / 3)
-
-    console.log(presentation_data)
-    for(let i = 0; i < n_presentations; i++){
+    console.log(room_iterators)
+    
+    for(let i = 0; i < n_presentation_rooms; i++){
         //read presentations
         var pres_names = document.getElementsByClassName("name")
         var pres_authors = document.getElementsByClassName("author")
         var pres_times = document.getElementsByClassName("time")
 
-        pres_names[i].innerHTML = presentation_data[i * 3]
-        pres_authors[i].innerHTML = presentation_data[i * 3 + 1]
-        pres_times[i].innerHTML = presentation_data[i * 3 + 2]
+        if (room_iterators[n_presentation_rooms] == presentation_data[i][0].length){ //there is no more presentations in spec_room
+            pres_names[i].style.display = "none"
+        }
+
+        pres_names[i].innerHTML = presentation_data[i][0][room_iterators[i]].title
+        pres_authors[i].innerHTML = presentation_data[i][0][room_iterators[i]].name
+        pres_times[i].innerHTML = presentation_data[i][0][room_iterators[i]].time
     }
 
 }
@@ -96,47 +123,56 @@ function update_time(){
     var progress = document.getElementsByClassName("myBar")
     var bars = document.getElementsByClassName("myProgress")
 
-    for (let i = 0; i < n_presentations; i++){
+    for (let i = 0; i < n_presentation_rooms; i++){
 
-        //check if presentation did not end
+        let string_end = times[i].innerHTML.slice(times[i].innerHTML.indexOf("-") + 1, times[i].innerHTML.length)
+        let string_start = times[i].innerHTML.slice(0, 6)
 
+        let time_end = ConvertStringIntoTime(string_end)
+        let time_start = ConvertStringIntoTime(string_start)
+        let time_curr = new Date()
 
-        let time_end = times[i].innerHTML.slice(times[i].innerHTML.indexOf("-") + 1, times[i].innerHTML.length)
-        let time_start = times[i].innerHTML.slice(0, 6)
+        let pres_len = time_end - time_start
+        let diff = time_end - time_curr
+        
+        let minutes_diff = Math.round(diff / 60000);
+        let minutes_length = Math.round(pres_len / 60000)
+        
+        let min_diff = Math.round(minutes_diff % 60)
+        let hour_diff = Math.round(minutes_diff / 60)
 
-        let start_hours = parseInt(time_start.slice(0, 3))
-        let start_minutes = parseInt(time_start.slice(5, 7))
-
-        let end_hours = parseInt(time_end.slice(0, 3))
-        let end_minutes = parseInt(time_end.slice(5, 7))
-
-        let curr_hours = parseInt(hours)
-        let curr_minutes = parseInt(minutes)
-
-        let m_diff = (curr_hours * 60 + curr_minutes) - (end_hours * 60 + end_minutes)
-        let pres_length = (start_hours * 60 + start_minutes) - (end_hours * 60 + end_minutes)
-
-        let sub_time
+        let min_diff_str = `${min_diff}`
+        if (min_diff_str.length == 1){
+            min_diff_str = `0${min_diff}`
+        }
 
         //now convert back to time string
-        if (m_diff / 60 < 1){
-            sub_time = `00:${Math.abs(m_diff)}`
+        if (hour_diff == 0){
+            sub_time = `00:${min_diff_str}`
         }
         else{
-            sub_time = `${Math.abs(Math.floor(m_diff / 60))}:${Math.abs(m_diff % 60)}`
+            sub_time = `${hour_diff}:${min_diff_str}`
+        }
+
+        if (min_diff < 10){
+        }
+
+        //check if presentation did not end
+        if (sub_time == "00:00"){
+            room_iterators[i] += 1 //increment
         }
         
         //update time_diff
         timers[i].innerHTML = `Zbývá: ${sub_time}`
 
+
         //update all progressbars
-        let infill = (m_diff / pres_length).toFixed(2)
-        let update_width = Math.round(bars[i].offsetWidth * infill)
-        if (update_width > 100){
+        let infill = (1 - (minutes_diff / minutes_length)).toFixed(2)
+        if (infill > 1.00){
             console.log("something is broken")
-            return
+            return 
         }
-        progress[i].style.width = update_width + "%"
+        progress[i].style.width = (infill * 100) + "%"
     }
 }
 
